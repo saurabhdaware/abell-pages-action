ce8997e6918076e8fb7f3ea85ac7f8372385e88c
Saurabh Daware <saurabhdaware99@gmail.com>
2020-08-28 21:13:07 +0530

    add themePath option

--- index.js
+++ index.js
@@ -3,10 +3,9 @@ const github = require('@actions/github');
 
 try {
   // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
+  const themePath = core.getInput('template-path');
+  console.log(`Theme Path: ${themePath}!`);
+  console.log(__dirname);
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
