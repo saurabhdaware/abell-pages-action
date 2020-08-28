783aa963861fa15c4619534d81b9297ee754a47e
Saurabh Daware <saurabhdaware99@gmail.com>
2020-08-28 21:34:48 +0530

    read readme

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
+const fs = require('fs');
 const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
   // `who-to-greet` input defined in action metadata file
-  const themePath = core.getInput('template-path');
+  const themePath = core.getInput('theme-path');
   console.log(`Theme Path: ${themePath}!`);
-  console.log(__dirname);
+  console.log(fs.readFileSync('./README.md', 'utf-8'));
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
